0a01d0a4d296792ae4f99a3fee7ad68fdbf5c723
jamesrothmann <35830265+jamesrothmann@users.noreply.github.com>
2022-12-14 07:11:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,6 +76,10 @@ def gpt3_summarize(paragraph):
         prompt=f"TL;DR: {paragraph_text}",
         max_tokens=1024,
         temperature=0.5,
+        top_p=1,
+        best_of=1,
+        frequency_penalty=0,
+        presence_penalty=0
     )
 
     # Return the summary text
